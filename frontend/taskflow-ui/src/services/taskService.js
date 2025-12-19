110d16f3bdf23be1889ffd95412d14ca5d2d9f14
RudraRajdev <rudra51951@gmail.com>
2025-12-19 22:29:38 +0530

    Update taskService.js

--- frontend/taskflow-ui/src/services/taskService.js
+++ frontend/taskflow-ui/src/services/taskService.js
@@ -4,20 +4,20 @@ const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 // GET all tasks
 export const getTasks = () =>
-  axios.get(`${API_URL}/task`);
+  axios.get(`${API_URL}task`);
 
 // ADD task
 export const addTask = (task) =>
-  axios.post(`${API_URL}/task/add`, task);
+  axios.post(`${API_URL}task/add`, task);
 
 // DELETE all tasks
 export const deleteAll = () =>
-  axios.delete(`${API_URL}/task/deleteAll`);
+  axios.delete(`${API_URL}task/deleteAll`);
 
 // DELETE single task
 export const deleteTask = (id) =>
-  axios.delete(`${API_URL}/task/delete/${id}`);
+  axios.delete(`${API_URL}task/delete/${id}`);
 
 // UPDATE task
 export const updateTask = (id, updatedTask) =>
-  axios.put(`${API_URL}/task/${id}`, updatedTask);
+  axios.put(`${API_URL}task/${id}`, updatedTask);
